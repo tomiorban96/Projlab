7d7c1dc8c0a27c47eacfbdc7209a95d253c488a0
kovacsgergelymusic <kovacsgergelymusic@gmail.com>
2018-03-18 20:48:49 +0100

    v2

--- Project/src/main/Field.java
+++ Project/src/main/Field.java
@@ -4,27 +4,58 @@ import java.util.ArrayList;
 
 public class Field {
 
-    protected ArrayList<Thing> movable;
-    private Field neighbor[];
+    protected Thing movable;
+    private Hashtable<Direction,Field> neighbors[];
 
     public boolean Accept(Player p, Player pusher, Direction d){
-        return false;
+        System.out.println("Field.Accept()");
+         if (movable!=null){                                     //van rajta valami
+            if (movable.PushedBy(pusher,p,d)){                  //ha el lehet tolni
+                System.out.println("Field.Accept() returns true");
+                return true;
+            }
+            else{                                               //ha nem lehet eltolni
+                System.out.println("Field.Accept() returns false");
+                return false;
+            }     
+        }
+        else{                                                   //nincs rajta semmi
+            System.out.println("Field.Accept() returns true");
+            return true;
+        }
     }
 
     public boolean Accept(Box b, Player pusher){
-        return false;
+        System.out.println("Field.Accept()");
+         if (movable!=null){                                     //van rajta valami
+            if (movable.PushedBy(pusher,b,d)){                  //ha el lehet tolni
+                System.out.println("Field.Accept() returns true");
+                return true;
+            }
+            else{                                               //ha nem lehet eltolni
+                System.out.println("Field.Accept() returns false");
+                return false;
+            }     
+        }
+        else{                                                   //nincs rajta semmi
+            System.out.println("Field.Accept() returns true");
+            return true;
+        }
     }
 
     public void Remove(Thing t){
-
+        System.out.println("Field.Remove()");
+        movable=null;
     }
 
     public Field GetNeighbor(Direction d){
-        return neighbor[d.ordinal()];
+       System.out.println("Field.GetNeighbor()");
+       System.out.println("Field.GetNeighbor() returns");
+       return neighbors.get(d);
     }
 
     Field(){
-        movable = new ArrayList<>();
-        neighbor = new Field[4];
+        movable = null;
+        neighbors = new Hashtable<Direction,Field>();
     }
 }
