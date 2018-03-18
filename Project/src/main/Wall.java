5f52f2d2511a07ae1cd613b909257a0c148caabe
kovacsgergelymusic <kovacsgergelymusic@gmail.com>
2018-03-18 17:46:09 +0100

    v1

--- Project/src/main/Wall.java
+++ Project/src/main/Wall.java
@@ -4,13 +4,13 @@ public class Wall extends Field{
 
     public boolean Accept(Player p, Player pusher){
         System.out.println("Wall.Accept()");
-        System.out.println("Wall.Accept() returns");
+        System.out.println("Wall.Accept() returns false");
         return false;
     }
 
     public boolean Accept(Box b, Player pusher){
         System.out.println("Wall.Accept()");
-        System.out.println("Wall.Accept() returns");
+        System.out.println("Wall.Accept() returns false");
         return false;
     } 
 }
