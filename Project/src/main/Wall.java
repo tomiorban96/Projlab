36dff70a29160b121e1c84ed065c36cd59af911d
kovacsgergelymusic <kovacsgergelymusic@gmail.com>
2018-03-18 16:20:38 +0100

    outputok

--- Project/src/main/Wall.java
+++ Project/src/main/Wall.java
@@ -3,10 +3,14 @@ package main;
 public class Wall extends Field{
 
     public boolean Accept(Player p, Player pusher){
+        System.out.println("Wall.Accept()");
+        System.out.println("Wall.Accept() returns");
         return false;
     }
 
     public boolean Accept(Box b, Player pusher){
+        System.out.println("Wall.Accept()");
+        System.out.println("Wall.Accept() returns");
         return false;
     }
 }
