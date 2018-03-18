2bd3ab0f19f967a2afbf1c5aeadb03c7ea9bc6e2
kovacsgergelymusic <kovacsgergelymusic@gmail.com>
2018-03-18 18:18:43 +0100

    v1

--- Project/src/main/Hole.java
+++ Project/src/main/Hole.java
@@ -4,19 +4,60 @@ public class Hole extends Field {
 
     private boolean open;
 
-    public boolean Accept(Player p, Player pusher){
-        return false;
+    public boolean Accept(Player p, Player pusher, Direction d){
+         if (movable!=null){                                     //van rajta valami
+            if (movable.PushedBy(pusher,b,d)){                  //ha el lehet tolni
+                if (open){                                      // ha a lyuk nyitva van
+                    p.Die();
+                }
+                System.out.println("Hole.Accept() returns true");
+                return true;
+            }
+            else{                                               //ha nem lehet eltolni
+                System.out.println("Hole.Accept() returns false");
+                return false;
+            }     
+        }
+        else{                                                   //nincs rajta semmi
+            if (open){                                          // ha a lyuk nyitva van
+                p.Die();
+            }
+            System.out.println("Hole.Accept() returns true");
+            return true;
+        }
     }
 
-    public boolean Accept(Box b, Player pusher){
-        return false;
+    public boolean Accept(Box b, Player pusher,Direction d){        //doboz mozog rá
+                                                              
+        if (movable!=null){                                     //van rajta valami
+            if (movable.PushedBy(pusher,b,d)){                  //ha el lehet tolni
+                if (open){                                      // ha a lyuk nyitva van
+                    b.Die();
+                }
+                System.out.println("Hole.Accept() returns true");
+                return true;
+            }
+            else{                                               //ha nem lehet eltolni
+                System.out.println("Hole.Accept() returns false");
+                return false;
+            }     
+        }
+        else{                                                   //nincs rajta semmi
+            if (open){                                          // ha a lyuk nyitva van
+                b.Die();
+            }
+            System.out.println("Hole.Accept() returns true");
+            return true;
+        }
     }
-
     public boolean GetOpen(){
+        System.out.println("Hole.GetOpen()");
+        System.out.println("Hole.GetOpen() returns");
         return open;
     }
 
     public void SetOpen(boolean b){
+        System.out.println("Hole.SetOpen()");
         open = b;
     }
 }
