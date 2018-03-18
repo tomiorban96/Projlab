02995dce4e0c69869e59139821f33329625999f1
kovacsgergelymusic <kovacsgergelymusic@gmail.com>
2018-03-18 17:44:55 +0100

    v1

--- Project/src/main/Switch.java
+++ Project/src/main/Switch.java
@@ -4,11 +4,41 @@ public class Switch extends Field {
 
     private Hole hole;
 
-    public boolean Accept(Player p, Player pusher){
-        return false;
+    public boolean Accept(Player p, Player pusher, Direction d){
+        System.out.println("Switch.Accept()");
+        if (movable!=null){                                     //ha van rajta valami
+            if (movable.PushedBy(pusher,b,d)){                  //ha azt el lehet tolni
+                System.out.println("Switch.Accept() returns");
+                return true;    
+            }
+            else{                                               //ha azt nem lehet eltolni 
+                System.out.println("Switch.Accept() returns");             
+                return false;
+            }
+        }
+        else{                                                  //ha nincs rajta semmi
+            System.out.println("Switch.Accept() returns");
+            return true;
+        }
     }
 
-    public boolean Accept(Box b, Player pusher){
-        return false;
+    public boolean Accept(Box b, Player pusher, Direction d){
+        System.out.println("Switch.Accept()");
+        if (movable!=null){                                     //ha van rajta valami
+            if (movable.PushedBy(pusher,b,d)){                  //ha azt el lehet tolni
+                hole.SetOpen(true);
+                System.out.println("Switch.Accept() returns");
+                return true;
+            }
+            else {                                              //ha azt nem lehet eltolni
+                System.out.println("Switch.Accept() returns");
+                return false;
+            }
+        }
+        else {                                                  //ha nincs rajta semmi
+            hole.SetOpen(true);
+            System.out.println("Switch.Accept() returns");
+            return true;
+        }
     }
 }
