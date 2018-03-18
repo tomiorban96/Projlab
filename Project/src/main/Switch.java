abf447e92c17a14bcf372a511e9c0ffce12b051f
kovacsgergelymusic <kovacsgergelymusic@gmail.com>
2018-03-18 17:47:27 +0100

    v1

--- Project/src/main/Switch.java
+++ Project/src/main/Switch.java
@@ -8,16 +8,16 @@ public class Switch extends Field {
         System.out.println("Switch.Accept()");
         if (movable!=null){                                     //ha van rajta valami
             if (movable.PushedBy(pusher,b,d)){                  //ha azt el lehet tolni
-                System.out.println("Switch.Accept() returns");
+                System.out.println("Switch.Accept() returns true");
                 return true;    
             }
             else{                                               //ha azt nem lehet eltolni 
-                System.out.println("Switch.Accept() returns");             
+                System.out.println("Switch.Accept() returns false");             
                 return false;
             }
         }
         else{                                                  //ha nincs rajta semmi
-            System.out.println("Switch.Accept() returns");
+            System.out.println("Switch.Accept() returns true");
             return true;
         }
     }
@@ -27,17 +27,17 @@ public class Switch extends Field {
         if (movable!=null){                                     //ha van rajta valami
             if (movable.PushedBy(pusher,b,d)){                  //ha azt el lehet tolni
                 hole.SetOpen(true);
-                System.out.println("Switch.Accept() returns");
+                System.out.println("Switch.Accept() returns true");
                 return true;
             }
             else {                                              //ha azt nem lehet eltolni
-                System.out.println("Switch.Accept() returns");
+                System.out.println("Switch.Accept() returns false");
                 return false;
             }
         }
         else {                                                  //ha nincs rajta semmi
             hole.SetOpen(true);
-            System.out.println("Switch.Accept() returns");
+            System.out.println("Switch.Accept() returns true");
             return true;
         }
     }
