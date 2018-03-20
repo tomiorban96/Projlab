30d10093bb1e080f05f6ba94102d72edf9c33300
kovacsgergelymusic <kovacsgergelymusic@gmail.com>
2018-03-20 01:25:40 +0100

    feltöltött

--- Project/src/main/Box.java
+++ Project/src/main/Box.java
@@ -1,23 +1,20 @@
 package main;
 
 public class Box extends Thing{
-	
-
-	
 	 /**
 	 * A Box meghal, vagyis eltûnik a pályáról. Meghívja a field Remove függvényét, anélkül hogy elõször áthelyezné magát
 	 *  másvhova. Ezzel kikerül a játékból. Csökkenti az aktív dobozok számát, és ellenőrizteti, hogy vége van-e a játéknak.
 	 */
-   public void Die(){
-   	System.out.println("Box.Die()");
-   	field.Remove(this);
-   	game.DecActiveBoxes();
+  public void Die(){
+  	System.out.println("Box.Die()");
+  	field.Remove(this);
+  	game.DecActiveBoxes();
 	game.CheckEndGame();
-   	System.out.println("Box.Die() returns");
-   	return;
+  	System.out.println("Box.Die() returns");
+  	return;
 
-   }
-   /**
+  }
+  /**
 	 * A Boxot tolják egy irányba. 
 	 * Ha a Boxot közvetve tolják akkor meghívja a következő Field Accept-jét. Ha az true-val tér vissza akkor kiveszi magát az eddigi Field-ről
 	 * a Remove-val és visszatér true-val, ha false-al akkor visszatér false-al.
@@ -26,18 +23,18 @@ public class Box extends Thing{
 	 * @param d az a irány amerre a játékost tolják
 	 * @return sikerült-e a Player-nek elmennie a fieldről
 	 */
-   public boolean PushedBy(Player p, Thing t, Direction d){
-   	System.out.println("Box.Pushedby(p,t,d)");
-   	Field currentField=field;
+  public boolean PushedBy(Player p, Thing t, Direction d){
+  	System.out.println("Box.Pushedby(p,t,d)");
+  	Field currentField=field;
 		Field neighbor=field.GetNeighbor(d);
 		if(neighbor.Accept(this,p, d)) {
 			currentField.Remove(this);
-			System.out.println("Player.Pushedby(,p,t,d) returns");
+			System.out.println("Player.Pushedby(,p,t,d) returns true");
 			return true;
 		}
 		else {
-			System.out.println("Player.Pushedby(p,t,d) returns");
-			return true;
+			System.out.println("Player.Pushedby(p,t,d) returns true");
+			return false;
 		}
-   }
+  }
 }
