7690ded4ce1bd6b780b4c7c8e9b17d16eb5a1d7f
kovacsgergelymusic <kovacsgergelymusic@gmail.com>
2018-03-20 01:32:23 +0100

    feltöltött

--- Project/src/main/Thing.java
+++ Project/src/main/Thing.java
@@ -2,43 +2,19 @@ package main;
 
 public abstract class Thing {
 	/**
-    * Az a mező amin a Thing van
-    */
+	    * Az a mező amin a Thing van
+	    */
     protected Field field;
     /**
      * A játék amiben a thing részt vesz
      */
     protected Game game;
     /**
-	 * @return the field
-	 */
-	public Field getField() {
-		return field;
-	}
-	/**
-	 * @param field the field to set
-	 */
-	public void setField(Field field) {
-		this.field = field;
-	}
-	/**
-	 * @return the game
-	 */
-	public Game getGame() {
-		return game;
-	}
-	/**
-	 * @param game the game to set
-	 */
-	public void setGame(Game game) {
-		this.game = game;
-	}
-	/**
      * Absztrakt függvény amit a leszármazottak valósítanak meg
      */
-    abstract void Die(); 
+    abstract void Die();
     /**
-     * Absztrakt függvény amit a leszármazottak valósítanak meg
+     * Akkor hívódik, ha tolni próbálják a Thinget.
      * @param p játékos aki tol
      * @param t Thing ami közvetlen tol (t==p lehet)
      * @param d a tolás iránya
@@ -49,10 +25,16 @@ public abstract class Thing {
         System.out.println("Thing.PushedBy() returns false");
         return false; 
     }
-     public void SetField(Field field) {
-    	this.field=field;
-    }
-    public void SetGame(Game game){
-        this.game = game;
-    }
+    /**
+	 * @param field the field to set
+	 */
+	public void setField(Field field) {
+		this.field = field;
+	}/**
+	 * @param game the game to set
+	 */
+	public void setGame(Game game) {
+		this.game = game;
+	}
+    
 }
