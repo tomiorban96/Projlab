e10cb62dfaca4e7669509050b2e5e75a4d0a3ffd
kovacsgergelymusic <kovacsgergelymusic@gmail.com>
2018-03-20 01:27:57 +0100

    feltöltött

--- Project/src/main/Game.java
+++ Project/src/main/Game.java
@@ -15,44 +15,8 @@ public class Game {
      * a játékban lévő aktív Box-ok száma,
      * egy Box aktív ha nincs PointField-en és lehet tolni
      */
-    private int ActiveBoxes;
+    private int ActiveBoxes; 
     /**
-	 * @return the board
-	 */
-	public Board getBoard() {
-		return board;
-	}
-	/**
-	 * @param board the board to set
-	 */
-	public void setBoard(Board board) {
-		this.board = board;
-	}
-	/**
-	 * @return the players
-	 */
-	public ArrayList<Player> getPlayers() {
-		return players;
-	}
-	/**
-	 * @param players the players to set
-	 */
-	public void setPlayers(ArrayList<Player> players) {
-		this.players = players;
-	}
-	/**
-	 * @return the activeBoxes
-	 */
-	public int getActiveBoxes() {
-		return ActiveBoxes;
-	}
-	/**
-	 * @param activeBoxes the activeBoxes to set
-	 */
-	public void setActiveBoxes(int activeBoxes) {
-		ActiveBoxes = activeBoxes;
-	}
-	/**
      * Elindítja a játékot.
      */
     public void StartGame(){
@@ -71,16 +35,16 @@ public class Game {
     }
     /**
      * Megnézi vége van-e a játéknak.
-     * Akkor van vége ha nincsenek aktív Boxok, vagy élő játékosok
+     * Akkor van vége ha nincsenek aktív Boxok, vagy élő játékosok.
      */
     public boolean CheckEndGame(){
         System.out.println("Game.CheckEndGame()");
-        if (ActiveBoxes==0|LastPlayer()){
-            System.out.println("Game.CheckEndGame() returns");
+        if (ActiveBoxes==0||LastPlayer()){
+            System.out.println("Game.CheckEndGame() returns true");
             return true;
         }
         else{
-            System.out.println("Game.CheckEndGame() returns");
+            System.out.println("Game.CheckEndGame() returns false");
             return false;
         }
     }
@@ -89,17 +53,41 @@ public class Game {
      */
     public void DecActiveBoxes(){
         System.out.println("Game.DecActiveBoxes()");
-        //ActiveBoxes--;
+        ActiveBoxes--;
     }
+    /**
+     * Megnézi, hogy van-e még élő játékos.
+     */
     private boolean LastPlayer() {
     	int n=0;
     	for (Player p:players) {
-    		if (p.GetAlive())
+    		if (p.getAlive())
     			n++;
     	}
-    	if (n<=1)
+    	if (n<1)
     		return true;
     	else
     		return false;
     }
+    /**
+	 * @param board the board to set
+	 */
+	public void setBoard(Board board) {
+		this.board = board;
+	}
+	/**
+	 * @param players the players to set
+	 */
+	public void setPlayers(ArrayList<Player> players) {
+		this.players = players;
+	}
+	/**
+	 * @param activeBoxes the activeBoxes to set
+	 */
+	public void setActiveBoxes(int activeBoxes) {
+		ActiveBoxes = activeBoxes;
+	}
+	public ArrayList<Player> getPlayers() {
+		return players;
+	}
 }
