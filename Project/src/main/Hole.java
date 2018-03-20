d669e29dc0512c76f1225d4558f559b8bc28750e
kovacsgergelymusic <kovacsgergelymusic@gmail.com>
2018-03-20 01:28:27 +0100

    feltöltött

--- Project/src/main/Hole.java
+++ Project/src/main/Hole.java
@@ -6,20 +6,21 @@ public class Hole extends Field {
      */
     private boolean open;
     /**
-    * Ha a movable null, felveszi a p:Player játékost a movable tagváltozónak, és meghívja a die függvényét. Ekkor true-t ad vissza.
-    * Ha a movable nem null akkor először meghívja a movable pushedBy(pusher,p,d) függvényét. Ha az true-val tér vissza
-    * akkor elhelyezi a p-t a movable tagváltozóban. 
-    * Ekkor ha az open az true akkor meghívja a p die() függvényét. Ha false akkor nem. Mindkét esetben visszatér true-val
-    * Ha a PushedBy false-al tér vissza akkor nem helyezi el p-t és false-al tér vissza.
-    * @param p az a Player aki közvetlen rá akar lépni a mezőre
-    * @param pusher az a Player aki eredetileg tolta a p-t
-    * @param d az az irány amerre tolódnia kell a movable-nek ha az nem null
-    * @return sikerült-e elhelyezni a p-t a mezőn
-     */
+     * Ha a movable null, felveszi a p:Player játékost a movable tagváltozónak, és meghívja a die függvényét. Ekkor true-t ad vissza.
+     * Ha a movable nem null akkor először meghívja a movable pushedBy(pusher,p,d) függvényét. Ha az true-val tér vissza
+     * akkor elhelyezi a p-t a movable tagváltozóban. 
+     * Ekkor ha az open az true akkor meghívja a p die() függvényét. Ha false akkor nem. Mindkét esetben visszatér true-val
+     * Ha a PushedBy false-al tér vissza akkor nem helyezi el p-t és false-al tér vissza.
+     * @param p az a Player aki közvetlen rá akar lépni a mezőre
+     * @param pusher az a Player aki eredetileg tolta a p-t
+     * @param d az az irány amerre tolódnia kell a movable-nek ha az nem null
+     * @return sikerült-e elhelyezni a p-t a mezőn
+      */
     public boolean Accept(Player p, Player pusher, Direction d){
         System.out.println("Hole.Accept()");
-         if (movable!=null){                                     //van rajta valami
+         if (movable!=null){                                    //van rajta valami
             if (movable.PushedBy(pusher,p,d)){                  //ha el lehet tolni
+            	
                 if (open){                                      // ha a lyuk nyitva van
                     p.Die();
                 }
@@ -32,6 +33,7 @@ public class Hole extends Field {
             }     
         }
         else{                                                   //nincs rajta semmi
+        	
             if (open){                                          // ha a lyuk nyitva van
                 p.Die();
             }
@@ -52,23 +54,25 @@ public class Hole extends Field {
       */
     public boolean Accept(Box b, Player pusher,Direction d){        //doboz mozog rá
         System.out.println("Hole.Accept()");                                                     
-        if (movable!=null){                                     //van rajta valami
-            if (movable.PushedBy(pusher,b,d)){                  //ha el lehet tolni
-                if (open){                                      // ha a lyuk nyitva van
+        if (movable!=null){                                     	//van rajta valami
+            if (movable.PushedBy(pusher,b,d)){                 		//ha el lehet tolni
+            	
+                if (open){                                      	// ha a lyuk nyitva van
                     b.Die();
                 }
                 System.out.println("Hole.Accept() returns true");
                 return true;
             }
-            else{                                               //ha nem lehet eltolni
+            else{                                               	//ha nem lehet eltolni
                 System.out.println("Hole.Accept() returns false");
                 return false;
             }     
         }
-        else{                                                   //nincs rajta semmi
-            if (open){                                          // ha a lyuk nyitva van
+        else{                                                   	//nincs rajta semmi
+            if (open){                                          	// ha a lyuk nyitva van
                 b.Die();
             }
+            
             System.out.println("Hole.Accept() returns true");
             return true;
         }
@@ -88,6 +92,10 @@ public class Hole extends Field {
      */
     public void SetOpen(boolean b){
         System.out.println("Hole.SetOpen()");
+        if (b&&movable!=null) {
+        	movable.Die();
+        }
         open = b;
-    } 
+        System.out.println("Hole.SetOpen() returns");
+    }
 }
